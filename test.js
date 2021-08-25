d423207fec9fbf3207d12c85121cfbb619a71c3b
totoro65 <86171458+totoro65@users.noreply.github.com>
2021-08-26 01:04:18 +0200

    Delete test.js

--- test.js
+++
@@ -1,8 +0,0 @@
-let old = $('.card').get(0);
-$('.card').click(function(){
-  if(old!=null && $(old).hasClass('open'))
-    $(old).toggleClass('open');
-   $(this).toggleClass('open');
-   old = this;
-
-})
